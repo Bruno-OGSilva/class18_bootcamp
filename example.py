b88262785e7de88c0023b6e494a0cc7cf3676480
Bruno Silva <bruno.ogs@gmail.com>
2024-10-02 22:41:31 -0400

    update

---
+++ example.py
@@ -0,0 +1,24 @@
+import requests
+from pydantic import BaseModel
+
+class PokemonSchema(BaseModel): #schema de dados
+    name: str
+    type: str
+
+    class Config:
+        from_attributes = True
+
+def pegar_pokemon(id: int) -> PokemonSchema:
+    response = requests.get(f"https://pokeapi.co/api/v2/pokemon/{id}")
+    data = response.json()
+    data_types = data['types']  # Supondo que 'data' é o dicionário com os dados do Pokémon
+    types_list = []
+    for type_info in data_types:
+        types_list.append(type_info['type']['name'])
+    types = ', '.join(types_list)
+    return PokemonSchema(name=data['name'], type=types)
+
+if __name__ == "__main__":
+    print(pegar_pokemon(10))
+    print(pegar_pokemon(6))
+    print(pegar_pokemon(13))
